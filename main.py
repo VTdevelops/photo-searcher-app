a71e58386886d36f6fe7e66dc1f653eae47752d3
VTdevelops <117118443+VTdevelops@users.noreply.github.com>
2023-06-03 11:09:38 +0200

    build photo searcher app with kivy

---
+++ main.py
@@ -0,0 +1,48 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+import wikipedia
+import requests
+
+Builder.load_file('frontend.kv')
+
+
+class FirstScreen(Screen):
+
+    def get_image_link(self):
+        # get user query from TextInput
+        query = self.manager.current_screen.ids.user_query.text
+        # Get wikipedia page and the first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        return image_link
+
+    def download_image(self):
+        # Download the image
+        headers = {
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/87.0.4280.141 Safari/537.36"
+        }
+        req = requests.get(self.get_image_link(), headers=headers)
+        imagepath = 'files/image.jpg'
+        with open(imagepath, 'wb') as file:
+            file.write(req.content)
+        return imagepath
+
+    def set_image(self):
+        # Set the image in the Image widget
+        self.manager.current_screen.ids.img.source = self.download_image()
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+MainApp().run()
